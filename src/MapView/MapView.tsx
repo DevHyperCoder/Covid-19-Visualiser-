7ecf202e53a2fbe39b0e25551bd01429c3c4dbad
DevHyperCoder <arduinoleo88@gmail.com>
2020-09-05 10:06:46 +0530

    Add ReactMapboxGL

--- src/MapView/MapView.tsx
+++ src/MapView/MapView.tsx
@@ -1,11 +1,36 @@
-import React from 'react';
+import React, { useState } from "react";
+import ReactMapGL from "react-map-gl";
+import "mapbox-gl/dist/mapbox-gl.css";
 
-function MapView(){
-    return (
-        <div>
-            <h1>MapView</h1>
-        </div>
-    )
-}
+const TOKEN =
+  "MAPBOX_API_KEY";
 
-export default MapView
+
+
+const MapView = () => {
+  const [viewport, setViewPort] = useState({
+    width: "100%",
+    height: 900,
+    latitude: 0,
+    longitude: 0,
+    zoom: 2,
+  });
+
+  const _onViewportChange = (viewport:any) =>
+    setViewPort({ ...viewport});
+
+  return (
+    <div style={{ zIndex:-1 }}>
+      
+      <ReactMapGL
+        {...viewport}
+        mapboxApiAccessToken={TOKEN}
+        mapStyle="mapbox://styles/mapbox/light-v8"
+        onViewportChange={_onViewportChange}
+      >
+      </ReactMapGL>
+    </div>
+  );
+};
+
+export default MapView;
