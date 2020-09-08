9650b92f31b7542218b58fc470287b316ea7de3e
DevHyperCoder <arduinoleo88@gmail.com>
2020-09-08 11:04:22 +0530

    💄 Table styling ✨Display table

--- src/numbers/Numbers.tsx
+++ src/numbers/Numbers.tsx
@@ -1,11 +1,42 @@
-import  React from "react";
+import React from "react";
 
-function Numbers(){
-    return (
-        <div>
-            <h1>Numbers</h1>
-        </div>
-    )
+import TableHeader from "../TableHeader";
+import TableRow from "../TableData";
+import getMap from "../LatLongMap";
+import './Numbers.css'
+function Numbers({ json }: { json: any }) {
+  const map :string[][] = [[]];
+  const latLongMap = getMap();
+
+  for (let i = 0; i < json.length; i++) {
+    const currElem = json[i];
+    console.log(currElem);
+    const { name, active, cured } = currElem;
+
+    if (latLongMap.has(name)) {
+        map.push([name,active,cured])
+    }
+  }
+  return (
+    <div className="container">
+        <div className="table-container">
+      <h1>Numbers</h1>
+      <table>
+        <thead>
+          <tr>
+            <TableHeader
+              data={["State", "Active Cases", "Cured Cases"]}
+            ></TableHeader>
+          </tr>
+        </thead>
+        <tbody>
+          <TableRow data={map}></TableRow>
+        </tbody>
+      </table>
+    </div>
+    <p>All data taken from <a href="https:mohfw.gov.in/">mohfw.gov.in</a></p>
+    </div>
+  );
 }
 
 export default Numbers;
