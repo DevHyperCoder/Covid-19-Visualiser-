68ad45dd792fa479b6e3b140b4937064e385aed8
DevHyperCoder <arduinoleo88@gmail.com>
2020-09-08 11:02:13 +0530

    🎨 Add async data loading to App.tsx 🔥Remove MapViewWrapper (tsx and css)

--- src/App.tsx
+++ src/App.tsx
@@ -1,32 +1,48 @@
 import React from "react";
 import "./App.css";
 import Numbers from "./numbers/Numbers";
+import MapView from "./MapView/MapView";
 import Navbar from "./Navbar/Navbar";
-import MapViewWrapper from "./MapViewWrapper/MapViewWrapper";
-import {
-  BrowserRouter as Router,
-  Route,
-  Switch,
-} from "react-router-dom";
-function App() {
-  return (
-    <div>
-      <Router>
-      <Navbar></Navbar>
-      <main>
-          <Switch>
-            <Route path="/" exact={true}>
-              <MapViewWrapper/>
-            </Route>
-            <Route path="/numbers">
-              <Numbers />
-            </Route>
-          </Switch>
-      </main>
-      </Router>
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 
-    </div>
-  );
+interface Props {}
+
+interface State {
+  json: any;
 }
 
-export default App;
+export default class App extends React.Component<Props, State> {
+  constructor(props: any) {
+    super(props);
+    this.state = {
+      json: "",
+    };
+  }
+  async componentDidMount() {
+    const re = await fetch(
+      "https://covid19-india-adhikansh.herokuapp.com/states"
+    );
+    const json = (await re.json()).state;
+
+    this.setState({ json: json });
+  }
+  render() {
+    return (
+      <div>
+        <Router>
+          <Navbar />
+          <main className="main">
+            <Switch>
+              <Route path="/" exact={true}>
+                <MapView json={this.state.json} />
+              </Route>
+              <Route path="/numbers">
+                <Numbers json={this.state.json} />
+              </Route>
+            </Switch>
+          </main>
+        </Router>
+      </div>
+    );
+  }
+}
